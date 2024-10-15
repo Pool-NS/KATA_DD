06970c2feeedc9846a1d73b5226c889eb9e45e9c
Pool-NS <75177414@continental.edu.pe>
2024-10-15 14:48:28 -0500

    calcular promedio en (VERDE)

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -1,8 +1,7 @@
 class Conjunto:
     def __init__(self, conjunto):
-        self.__conjunto = conjunto
+        self._conjunto = conjunto
 
-    def promedio(self):
-        if not self.__conjunto:  # Si el conjunto está vacío
-            return None
-        return sum(self.__conjunto) / len(self.__conjunto)  # Calcular el promedio
+    def promedio (self):
+        return None
+
