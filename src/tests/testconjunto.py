07099fe69680e032288e25a60dac1f035111bcf4
POOL-NS <751777414@continental.edu.pe>
2024-10-21 22:45:55 -0500

    Configuración inicial del proyecto con clase Conjunto

--- src/tests/testconjunto.py
+++ src/tests/testconjunto.py
@@ -1,23 +1,15 @@
 import unittest
-from src.logica.Conjunto import Conjunto
+from src.logica.calculo import calcular_promedio,NoSePuedeCalcular
 
-class TestConjunto(unittest.TestCase):
+class TestCalculoPromedio(unittest.TestCase):
 
-    def test_conjunto_vacio_retornaNone(self):
-        conjunto = Conjunto([])
-        self.assertIsNone(conjunto.promedio())
+    def test_lista_vacia(self):
+        # Lista vacía debe lanzar NoSePuedeCalcular
+        with self.assertRaises(NoSePuedeCalcular):
+            calcular_promedio([])
 
-    def test_conjunto_unElemento_retornaValorUnicoElemento(self):
-        conjunto = Conjunto([5])
-        self.assertEqual(5, conjunto.promedio())
-
-    def test_conjunto_dosElementos_retornaPromedioElementos(self):
-        conjunto = Conjunto([5, 7])
-        self.assertEqual(6, conjunto.promedio())
-
-    def test_conjunto_nElementos_retornaPromedioNElementos(self):
-        conjunto = Conjunto([2, 4, 8, 9, 10, 15])
-        self.assertEqual((2 + 4 + 8 + 9 + 10 + 15) / 6, conjunto.promedio())
+if __name__ == '__main__':
+    unittest.main()
 
 
 
